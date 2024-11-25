f1e7076cdd785ed8b08d7f8757bf7e96b965fe8b
xmayeur <xavier@mayeur.be>
2024-11-25 22:39:22 +0100

    Adding invalid PDF file errors

--- pdf-merge.py
+++ pdf-merge.py
@@ -1,11 +1,13 @@
 import argparse
 import logging
+import warnings
 from getpass import getpass
 from glob import glob
 from sys import exit
 
 import pypdf
 
+warnings.filterwarnings("ignore")
 log = logging.getLogger(__name__)
 
 
@@ -34,15 +36,23 @@ def main():
     if args.combined:
         # read PDF documents
         try:
-            inp1 = pypdf.PdfReader(args.files[0])
+            inp1 = pypdf.PdfReader(args.files[0], strict=True)
         except FileNotFoundError:
             log.error(f"File {args.files[0]} not found")
             exit(-1)
+        except Exception as e:
+            log.warning(f'Invalid PDF file {args.files[0]} - {e}')
+            exit(-1)
+
         try:
-            inp2 = pypdf.PdfReader(args.files[1])
+            inp2 = pypdf.PdfReader(args.files[1], strict=True)
         except FileNotFoundError:
             log.error(f"File {args.files[1]} not found")
             exit(-1)
+        except Exception as e:
+            log.warning(f'Invalid PDF file {args.files[1]} - {e}')
+            exit(-1)
+
         pages = zip(inp1.pages, inp2.pages if args.no_reverse else inp2.pages[::-1])
         for page in pages:
             out.add_page(page[0])
@@ -55,7 +65,11 @@ def main():
             files = glob(args.files[0])
 
         for pdf in files:
-            out.append(pdf)
+            try:
+                out.append(pdf)
+            except Exception as e:
+                log.warning(f'Invalid PDF file {pdf} - {e}')
+                continue
 
     with open(args.output, "wb") as f:
         out.write(f)
